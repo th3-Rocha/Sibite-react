874f5d8dfce772518840de3f86482d1754fd03ea
th3-rocha <murilorocha537@gmail.com>
2022-12-31 11:34:35 -0300

    feat:created the logo

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import styled, { css } from 'styled-components'
+
+const Doidera = styled.p`  
+  color: palevioletred;
+`;
+
+const Container = styled.div`
+  text-align: center;
+`
+
+
+
+function App() {
+  return (
+    <Container>
+      <Doidera >Sibite test
+
+      </Doidera >
+    </Container>
+  );
+}
+
+export default App;
