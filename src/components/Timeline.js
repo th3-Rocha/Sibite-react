b41acb4b4b40fe02d5f530ba2cef3024ccae66fb
th3-rocha <murilorocha537@gmail.com>
2023-01-06 10:48:47 -0300

    feat: Need Polishe

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -5,10 +5,13 @@ import TimeHeader from '../subComponents/TimeHeader';
 import TimeSibitar from '../subComponents/TimeSibitar';
 import TimeShowMoreSibites from '../subComponents/TimeShowMoreSibites';
 import TimeSibiteView from '../subComponents/TimeSibiteView';
+import React, { useState, useEffect } from "react";
+
 const Timelinecontainner = styled.div`
   
   height: 100vh;
   min-width: 600px;
+  max-width: 600px;
   border-right: 1px;
   border-right-color: ${hexToRgba(COLORS.borderLine)};
   border-right-style: solid;
@@ -28,21 +31,60 @@ const Scroll = styled.div`
 
 
 function Menu() {
+  const [scrollDir, setScrollDir] = useState("scrolling down");
+
+  useEffect(() => {
+    const threshold = 0;
+    let lastScrollY = window.pageYOffset;
+    let ticking = false;
+  
+    const updateScrollDir = () => {
+      const scrollY = window.pageYOffset;
+  
+      if (Math.abs(scrollY - lastScrollY) < threshold) {
+        ticking = false;
+        return;
+      }
+      setScrollDir(scrollY > lastScrollY ? "scrolling down" : "scrolling up");
+      lastScrollY = scrollY > 0 ? scrollY : 0;
+      ticking = false;
+    };
+  
+    const onScroll = () => {
+      if (!ticking) {
+        window.requestAnimationFrame(updateScrollDir);
+        ticking = true;
+      }
+    };
+  
+    window.addEventListener("scroll", onScroll);
+    console.log(scrollDir);
+  
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [scrollDir]);
+
+  const element = document.getElementById('Scroller');
+  if (element) {
+    element.scroll(0, 100,{ behavior: 'smooth' });
+  }
+  console.log(scrollDir);
   return (
     <Timelinecontainner>
       <TimeHeader/>
-      <Scroll>
+      <Scroll id="Scroller">
         <TimeSibitar/>
         <TimeShowMoreSibites/>
+        
         {(() => {
-            const options = [];
+            const Sibites = [];
 
-            for (let i = 0; i <= 10; i++) {
-              options.push(<TimeSibiteView></TimeSibiteView>);
+            for (let i = 0; i <= 5; i++) {
+              Sibites.push(<TimeSibiteView sibiteNum = {`${i}`} ></TimeSibiteView>);
             }
 
-            return options;
+            return Sibites;
           })()}
+          
       </Scroll>
       
 
--- src/subComponents/TimeSibiteView.js
+++ src/subComponents/TimeSibiteView.js
@@ -1,15 +1,23 @@
 import styled, { css } from 'styled-components'
 import hexToRgba from 'hex-to-rgba';
 import {COLORS} from '../values/colors';
+import beckEnd from "../beckEnd.json";
 
+const SibiteContainner = styled.div`
 
-const SibiteContainner = styled.button`
-
-  background-color: yellow;
+  background-color: black;
   width: 100%;
-  min-height: 400px;
+  height: fit-content;
   color: white;
   border: 0px;
+  border-bottom: 1px;
+  border-style: solid;
+  border-color: ${hexToRgba(COLORS.borderLine)};
+  padding-top: 12px;
+  padding-left: 16px;
+  padding-right: 16px;
+  display: flex;
+  flex-direction: row;
   &:hover{
     background-color: ${hexToRgba(COLORS.darkTimelineHighlight)};
     cursor: pointer;
@@ -17,14 +25,139 @@ const SibiteContainner = styled.button`
   
   transition: 0.2s;
 `
+const ImageProfile = styled.img`
+    height: 48px;
+    border-radius: 100vw;
+    margin-right: 17px;
+    
+`
+const TextProfile = styled.div`
+   text-align: start;
+   width: max-content;
+`
+const TextProfileArroba = styled.p`
+   color:  ${hexToRgba(COLORS.backText)};
+   font-family: sans-serif;
+   font-weight: 100;
+   margin-left: 4px;
+`
 
-function TimeSibiteView() {
-      
-     
+const Hours = styled.p`
+   color:  ${hexToRgba(COLORS.backText)};
+   font-family: sans-serif;
+   font-weight: 100;
+   margin-left: 4px;
+`
+
+const SeparatorHours = styled.p`
+   color:  ${hexToRgba(COLORS.backText)};
+   font-family: sans-serif;
+   font-size: 15px;
+   font-weight: 500;
+   padding-left: 4px;
+`
+const FlexSeparator = styled.div`
+`
+
+const Content = styled.div`
+  margin-top: 5px;
+  margin-right: 16px;
+  p{
+    font-weight: 100;
+    margin-bottom: 10px;
+  }
+  img{
+    
+    border-radius:20px;
+    width:97%;
+    margin-bottom: 10px;
+  }
+`
+const AuthorName = styled.div`
+display: flex;
+flex-direction: row;
+font-size: 15px;
+align-items: center;
+`
+
+const Actions = styled.div`
+display: flex;
+width: 100%;
+margin-top: 0px;
+margin-bottom: 15px;
+`
+const ActionButton = styled.button`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  background-color: transparent;
+  border-style: none;
+  color: ${hexToRgba(COLORS.backText)};
+  align-items: center;
+  &:hover{
+      cursor: pointer;
+    }
+`
+
+function TimeSibiteView(props) {
+    const rndHour = Math.floor(Math.random() * 23) + 1;
+    const rndStats = Math.floor(Math.random() * 10000) + 1;
+    const rndComments = Math.floor(Math.random() * 1000) + 1;
+    const rndRe = Math.floor(Math.random() * 1000) + 1;
+    const rndLikes = Math.floor(Math.random() * 1000) + 1;
     return (
       
-      <SibiteContainner>         
-        
+      <SibiteContainner>   
+            <ImageProfile src={`https://github.com/${beckEnd.Sibites[props.sibiteNum].github}.png`} />
+            <FlexSeparator>
+
+
+              <AuthorName>
+                  <p>{`${beckEnd.Sibites[props.sibiteNum].nameAuthor}`}</p>
+                <TextProfileArroba>{`@${beckEnd.Sibites[props.sibiteNum].github}`}</TextProfileArroba>
+                <SeparatorHours>·</SeparatorHours>
+                <Hours>{rndHour} h</Hours>
+
+              </AuthorName>
+
+
+
+              <Content>
+                <p>{`${beckEnd.Sibites[props.sibiteNum].text}`}</p>
+                <img src = {`${beckEnd.Sibites[props.sibiteNum].mediaUrl}`} ></img>
+              
+              </Content>       
+
+              <Actions>
+
+                <ActionButton>
+                  <img src={process.env.PUBLIC_URL + "/assets/svg/stats.svg"} />
+                  <p>{rndStats}</p>
+                </ActionButton>
+
+                <ActionButton>
+                  <img src={process.env.PUBLIC_URL + "/assets/svg/comment.svg"} />
+                  <p>{rndComments}</p>
+                </ActionButton>
+
+                <ActionButton>
+                  <img src={process.env.PUBLIC_URL + "/assets/svg/ReSibite.svg"} />
+                  <p>{rndRe}</p>
+                </ActionButton>
+
+                <ActionButton>
+                  <img src={process.env.PUBLIC_URL + "/assets/svg/like.svg"} />
+                  <p>{rndLikes}</p>
+                </ActionButton>
+                
+                
+                <img src={process.env.PUBLIC_URL + "/assets/svg/shareSibite.svg"} />
+
+              </Actions>
+
+
+            </FlexSeparator>
+           
       </SibiteContainner>
     );
   }
